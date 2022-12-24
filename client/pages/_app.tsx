723b7cc505596082e2790e6d53aa3e80477bbbe3
Bas Kaal <3askaal@gmail.com>
2022-12-24 13:34:52 +0100

    clean up

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import 'normalize.css/normalize.css'
+import 'reset-css/reset.css'
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
