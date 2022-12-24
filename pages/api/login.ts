28e1e49316a1fe4eaad294293728b2578cc24be6
Bas Kaal <3askaal@gmail.com>
2022-12-24 14:46:49 +0100

    ditch nestjs server logic, move nextjs logic to root, clean up

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import querystring from 'querystring'
-import { generate as randomString } from 'randomstring'
+import { stringify as generateQueryString } from 'querystring'
+import { generate as generateRandomString } from 'randomstring'
 
 type Data = {
   name: string
@@ -11,11 +11,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  const state = randomString(16);
+  const state = generateRandomString(16);
 
   res.redirect(
     'https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
+    generateQueryString({
       response_type: "code",
       client_id: process.env.SPOTIFY_API_CLIENT_ID,
       scope: 'user-read-private user-read-email user-top-read',
