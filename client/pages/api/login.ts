c763d409dbe45ac7ae24879ff24529ebff9e77cd
Bas Kaal <3askaal@gmail.com>
2022-12-24 13:54:38 +0100

    add initial login logic

---
+++ client/pages/api/login.ts
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import querystring from 'querystring'
+import { generate as randomString } from 'randomstring'
+
+type Data = {
+  name: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const state = randomString(16);
+
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+    querystring.stringify({
+      response_type: "code",
+      client_id: process.env.SPOTIFY_API_CLIENT_ID,
+      scope: 'user-read-private user-read-email user-top-read',
+      redirect_uri: '',
+      state: state,
+    })
+  );
+}
