a33e3a360a4266184c3674bf3f5baae8444bad5a
Bas Kaal <3askaal@gmail.com>
2022-12-25 23:16:29 +0100

    process and visualise top genres data

--- components/collectIntelAuthenticated.tsx
+++ components/collectIntelAuthenticated.tsx
@@ -1,27 +1,32 @@
 import { useEffect, useState } from 'react'
-import { Spacer, Box, Container, Wrapper, Title, Input, Label, Row, Col } from '3oilerplate'
+import { Spacer, Box, Container, Wrapper, Title, Label } from '3oilerplate'
 import useSpotifyApi from '../hooks/useSpotifyApi'
+import { capitalize, flatten, uniq } from 'lodash'
 
 export default function CollectIntelAuthenticated() {
   const { spotifyApi, accessToken } = useSpotifyApi()
 
   const [topArtists, setTopArtists] = useState([])
+  const [topGenres, setTopGenres] = useState([])
   const [topTracks, setTopTracks] = useState([])
 
   useEffect(() => {
     if (!spotifyApi) return
 
-    spotifyApi.getMyTopArtists({ time_range: 'medium_term', limit: 10 })
+    spotifyApi.getMyTopArtists({ time_range: 'medium_term', limit: 50 })
       .then((data: any) => {
+        console.log(data?.body?.items)
         setTopArtists(data?.body?.items.map(({ name }: any) => ({
           name
         })))
+
+        setTopGenres(uniq(flatten(data?.body?.items.map(({genres}: any) => genres))))
       })
       .catch((err) => {
         console.log('ERR: ', err);
       })
 
-    spotifyApi.getMyTopTracks({ time_range: 'medium_term', limit: 10 })
+    spotifyApi.getMyTopTracks({ time_range: 'medium_term', limit: 50 })
       .then((data: any) => {
         setTopTracks(data?.body?.items.map(({ artists, name }: any) => ({
           artist: artists.map(({ name }: any) => name).join(', '),
@@ -39,6 +44,16 @@ export default function CollectIntelAuthenticated() {
       <Wrapper>
         <Container>
           <Spacer>
+          { !!topGenres.length && (
+              <Spacer>
+                <Title>Your Top Genres</Title>
+                <Box df fdr fww>
+                  { topGenres.map((genre, index) => (
+                    <Label sRef="Label" key={`artist-${index}`} s={{ mb: 's', mr: 's' }}>#{ index + 1 } { capitalize(genre) }</Label>
+                  )) }
+                </Box>
+              </Spacer>
+            )}
             { !!topArtists.length && (
               <Spacer>
                 <Title>Your Top Artists</Title>
