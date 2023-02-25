2e65a76818e4945c5074b525e97d894479a568a0
Bas Kaal <3askaal@gmail.com>
2023-02-25 22:07:02 +0100

    fix genre data formatting in new data structure, improve small ui stuff

--- client/components/collectIntelAuthenticated.tsx
+++ client/components/collectIntelAuthenticated.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useRef, useState } from 'react'
 import { Spacer, Box, Container, Wrapper, ElementGroup, Button } from '3oilerplate'
-import { orderBy, remove, startCase } from 'lodash'
+import { flatten, orderBy, remove, startCase, uniq } from 'lodash'
 import useSpotifyApi from '../hooks/useSpotifyApi'
 import SelectionLabel from './selectionLabel'
+import slugify from 'slugify'
 
 type DataTypes = 'artists' | 'tracks' | 'genres';
 type TermTypes = 'short_term' | 'medium_term' | 'long_term';
@@ -22,7 +23,7 @@ interface Intel {
 export default function CollectIntelAuthenticated() {
   const { spotifyApi, accessToken } = useSpotifyApi()
 
-  const [activeTab, setActiveTab] = useState<string>('artists')
+  const [activeTab, setActiveTabState] = useState<DataTypes>('artists')
   const [activeTerm, setActiveTerm] = useState<{ artists: TermTypes, tracks: TermTypes, genres: TermTypes }>({ artists: 'short_term', tracks: 'short_term', genres: 'short_term' })
 
   const [intel, setIntel] = useState<Intel>({
@@ -31,7 +32,6 @@ export default function CollectIntelAuthenticated() {
     genres: { short_term: [], medium_term: [], long_term: [] },
   })
 
-  // const topGenres = (term: TermTypes) => intel.genres
   const topTracks = intel.tracks ? intel.tracks[activeTerm.tracks] : []
   const topArtists = intel.artists ? intel.artists[activeTerm.artists] : []
   const topGenres = intel.genres ? intel.genres[activeTerm.genres] : []
@@ -51,13 +51,16 @@ export default function CollectIntelAuthenticated() {
         const accumulator = await accumulatorPromise
 
         const data = await spotifyApi[fetchers[instance]]({ time_range: term, limit: 50 })
-        const items = data?.body?.items.map(({ name, uri, artists }: any, index: number) => ({
+        const items = data?.body?.items.map(({ name, uri, artists, genres }: any, index: number) => ({
           id: uri,
           index,
           name,
-          selected: true,
+          include: true,
           ...(instance === 'tracks' && {
             artist: artists.map(({ name }: any) => name).join(', '),
+          }),
+          ...(instance === 'artists' && {
+            genres,
           })
         }))
 
@@ -67,23 +70,32 @@ export default function CollectIntelAuthenticated() {
         }
       }, Promise.resolve({ short_term: [], medium_term: [], long_term: [] }) as any)
 
-      // // includes alot of subgenres
-      // const uniqueGenres: string[] = uniq(flatten(items.map(({ genres }: any) => genres)))
+      let genres = {}
 
-      // // filter out subgenres
-      // const mainGenres = uniqueGenres
-      //   .filter((genre1) => uniqueGenres.some((genre2) => genre1 !== genre2 && genre2.includes(genre1)))
-      //   .map((genre) => ({
-      //     id: slugify(genre),
-      //     selected: true,
-      //     name: genre,
-      //   }))
+      if (instance === 'artists') {
+        genres = Object.entries(items).reduce((accumulator, [key, value]: any) => {
+          const uniqueGenres: string[] = uniq(flatten(value.map(({ genres }: any) => genres)))
+          const filteredGenres = uniqueGenres
+            .filter((genre1) => uniqueGenres.some((genre2) => genre1 !== genre2 && genre2.includes(genre1)))
+            .map((genre, index) => ({
+              id: slugify(genre),
+              include: true,
+              name: genre,
+              index
+            }))
+
+          return {
+            ...accumulator,
+            [key]: filteredGenres
+          }
+        }, {})
+      }
 
       return {
         ...accumulator,
         [instance]: items,
-        ...(instance === 'tracks' && {
-          // genres:
+        ...(instance === 'artists' && {
+          genres
         })
       }
     }, Promise.resolve({ tracks: {}, artists: {} }) as any)
@@ -91,57 +103,19 @@ export default function CollectIntelAuthenticated() {
     collectData().then((data) => {
       setIntel(data)
     })
-
-    // spotifyApi.getMyTopArtists({ time_range: 'medium_term', limit: 50 })
-    //   .then((data: any) => {
-    //     const artists = data?.body?.items.map(({ name, uri }: any, index: number) => ({
-    //       id: uri,
-    //       index,
-    //       name,
-    //       selected: true
-    //     }))
-    //     setTopArtists(artists)
-
-    //     // includes alot of subgenres
-    //     const uniqueGenres: string[] = uniq(flatten(data?.body?.items.map(({ genres }: any) => genres)))
-
-    //     // filter out subgenres
-    //     const mainGenres = uniqueGenres
-    //       .filter((genre1) => uniqueGenres.some((genre2) => genre1 !== genre2 && genre2.includes(genre1)))
-    //       .map((genre) => ({
-    //         id: slugify(genre),
-    //         selected: true,
-    //         name: genre,
-    //       }))
-
-    //     setTopGenres(mainGenres)
-    //   })
-    //   .catch((err) => {
-    //     console.log('ERR: ', err);
-    //   })
-
-    // spotifyApi.getMyTopTracks({ time_range: 'medium_term', limit: 50 })
-    //   .then((data: any) => {
-    //     setTopTracks(data?.body?.items.map(({ artists, name, uri }: any, index: number) => ({
-    //       id: uri,
-    //       index,
-    //       name,
-    //       artist: artists.map(({ name }: any) => name).join(', '),
-    //       selected: true,
-    //     })))
-    //   })
-    //   .catch((err) => {
-    //     console.log('ERR: ', err);
-    //   })
-
   }, [spotifyApi, accessToken])
+
+  const setActiveTab = (activeTab: DataTypes) => {
+    setActiveTabState(activeTab)
+    setActiveTerm({...activeTerm, [activeTab]: 'short_term'})
+  }
 
   const toggleItem = (type: DataTypes, term: TermTypes, id: string) => {
     const currentTypeData = intel[type]
     const currentTermData = currentTypeData ? currentTypeData[term] : []
 
     const currentItem = remove(currentTermData, { id })[0];
-    currentItem.selected = !currentItem.selected;
+    currentItem.include = !currentItem.include;
 
     const updatedTracks = orderBy(
       [...currentTermData, currentItem],
@@ -160,32 +134,32 @@ export default function CollectIntelAuthenticated() {
   }
 
   return (
-    <Spacer s={{ height: '100%' }}>
-      <ElementGroup>
-        <Button
-          isBlock
-          isOutline={activeTab !== 'artists'}
-          onClick={() => setActiveTab('artists')}
-        >
-          Artists
-        </Button>
-        <Button
-          isBlock
-          isOutline={activeTab !== 'tracks'}
-          onClick={() => setActiveTab('tracks')}
-        >
-          Tracks
-        </Button>
-        <Button
-          isBlock
-          isOutline={activeTab !== 'genres'}
-          onClick={() => setActiveTab('genres')}
-        >
-          Genres
-        </Button>
-      </ElementGroup>
+    <Spacer size="l" s={{ height: '100%' }}>
+      <Spacer>
+        <ElementGroup>
+          <Button
+            isBlock
+            isOutline={activeTab !== 'artists'}
+            onClick={() => setActiveTab('artists')}
+          >
+            Artists
+          </Button>
+          <Button
+            isBlock
+            isOutline={activeTab !== 'tracks'}
+            onClick={() => setActiveTab('tracks')}
+          >
+            Tracks
+          </Button>
+          <Button
+            isBlock
+            isOutline={activeTab !== 'genres'}
+            onClick={() => setActiveTab('genres')}
+          >
+            Genres
+          </Button>
+        </ElementGroup>
 
-      { (activeTab === 'artists' || activeTab === 'tracks' ) && (
         <ElementGroup s={{ alignSelf: 'center' }}>
           <Button
             isTermSelector
@@ -209,17 +183,34 @@ export default function CollectIntelAuthenticated() {
             Long Term
           </Button>
         </ElementGroup>
-      ) }
+      </Spacer>
+
 
       <Box df fdc s={{ flexGrow: 1, overflowY: 'auto', paddingY: 'm' }}>
+        { activeTab === 'genres' ? (
+          <Spacer>
+            <Box df fdr fww>
+              { topGenres.map(({ id, index, name, include }) => (
+                <SelectionLabel
+                  onClick={() => toggleItem('genres', activeTerm.genres, id)}
+                  key={`genre-${index}`}
+                  selected={include}
+                >
+                  { startCase(name) }
+                </SelectionLabel>
+              )) }
+            </Box>
+          </Spacer>
+        ) : null }
+
         { activeTab === 'artists' ? (
           <Spacer>
             <Box df fdr fww>
-              { topArtists.map(({ id, index, name, selected }) => (
+              { topArtists.map(({ id, index, name, include }) => (
                 <SelectionLabel
                   onClick={() => toggleItem('artists', activeTerm.artists, id)}
                   key={`artist-${index}`}
-                  selected={selected}
+                  selected={include}
                 >
                   { name }
                 </SelectionLabel>
@@ -231,29 +222,13 @@ export default function CollectIntelAuthenticated() {
         { activeTab === 'tracks' ? (
           <Spacer>
             <Box>
-              { topTracks.map(({ id, index, artist, name, selected }) => (
+              { topTracks.map(({ id, index, artist, name, include }) => (
                 <SelectionLabel
                   onClick={() => toggleItem('tracks', activeTerm.tracks, id)}
                   key={`track-${index}`}
-                  selected={selected}
+                  selected={include}
                 >
                   { artist } - { name }
-                </SelectionLabel>
-              )) }
-            </Box>
-          </Spacer>
-        ) : null }
-
-        { activeTab === 'genres' ? (
-          <Spacer>
-            <Box df fdr fww>
-              { topGenres.map(({ id, index, name, selected }) => (
-                <SelectionLabel
-                  onClick={() => toggleItem('genres', activeTerm.genres, id)}
-                  key={`genre-${index}`}
-                  selected={selected}
-                >
-                  { startCase(name) }
                 </SelectionLabel>
               )) }
             </Box>
